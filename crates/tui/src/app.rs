4644a78ea7bda49944baf3b660c31733004c7983
yukii <lichtestv@gmail.com>
2025-07-13 13:20:13 +0200

    Started with commands, saving and quitting works

--- crates/tui/src/app.rs
+++ crates/tui/src/app.rs
@@ -17,10 +17,6 @@ pub fn run_editor<B: Backend>(terminal: &mut Terminal<B>, editor: &mut Editor) -
                 continue;
             }
 
-            if key.code == KeyCode::Char('q') {
-                return Ok(true);
-            } 
-
             match editor.mode {
                 EditorMode::Normal => {
                     editor.handle_normal_mode_input(key.code);
@@ -35,9 +31,57 @@ pub fn run_editor<B: Backend>(terminal: &mut Terminal<B>, editor: &mut Editor) -
                 },
 
                 EditorMode::Command => {
-                    editor.handle_command_mode_input(key.code);
-                },
-                _ => {}
+                    if key.code == KeyCode::Esc {
+                        editor.change_mode(EditorMode::Normal);
+                        continue;
+                    }
+
+                    let mut command: String = String::new();
+
+                    if let KeyCode::Char(c) = key.code {
+                        command.push(c);
+                    }
+
+                    loop {
+                        if let Event::Key(next_key) = event::read()? {
+                            if next_key.kind == KeyEventKind::Release {
+                                continue;
+                            }
+
+                            match next_key.code {
+                                KeyCode::Enter => break,
+                                KeyCode::Esc => {
+                                    editor.change_mode(EditorMode::Normal);
+                                    break;
+                                }
+                                KeyCode::Char(c) => {
+                                    command.push(c);
+                                }
+                                KeyCode::Backspace => {
+                                    command.pop();
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
+
+                    match command.as_str() {
+                        "q" => return Ok(true),
+                        "w" => match editor.save_buffer() {
+                            Ok(_) => {
+
+                            }
+                            Err(_) => {}
+                        },
+                        "wq" => match editor.save_buffer() {
+                            Ok(_) => return Ok(true),
+                            Err(_) => {}
+                        }
+                        _ => {}
+                    }
+
+                    editor.change_mode(EditorMode::Normal); 
+                }
             }
         }
     }
